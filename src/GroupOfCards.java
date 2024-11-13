e471c169f528f4d323f6b666f89c391e7ff5f289
preet <damadama@sheridancollege.ca>
2024-11-12 23:20:50 -0500

    generated folder

--- src/GroupOfCards.java
+++ src/GroupOfCards.java
@@ -3,8 +3,9 @@
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
  */
-package ca.sheridancollege.project;
 
+
+import del2.Card;
 import java.util.ArrayList;
 import java.util.Collections;
 
--- src/del2/Deck.java
+++ src/del2/Deck.java
@@ -1,9 +1,12 @@
+package del2;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package deliverable1;
 
+
+import del2.Card;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
